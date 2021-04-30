53c2bf645c90d1457c6051fabc0039e3ae937087
Misaki <80904934+misaki0710@users.noreply.github.com>
2021-04-30 22:13:35 +0900

    Update Sort01.java
    Fix garbled characters

--- Sort01.java
+++ Sort01.java
@@ -8,7 +8,7 @@ public class Sort01{
 			System.out.print(array[i]);
 		}
 		
-		//¬‚³‚¢‡
+		//Ascending
 		for(int i = 0; i < array.length-1; i++){
 			for(int j = array.length-1; j > i; j--){
 				if(array[j] < array[j-1]){
@@ -23,7 +23,7 @@ public class Sort01{
 			System.out.print(array[i]);
 		}
 		
-		//‘å‚«‚¢‡
+		//Descending
 		for(int i = 0; i < array.length-1; i++){
 			for(int j = array.length-1; j > i; j--){
 				if(array[j] > array[j-1]){
