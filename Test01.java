4b3f3398e6216b7833c44f4c2fc6d50ba9883d46
Misaki <80904934+misaki0710@users.noreply.github.com>
2021-04-30 22:22:27 +0900

    Add files via upload

---
+++ Test01.java
@@ -0,0 +1,14 @@
+public class Test01{
+	public static void main(String[] args){
+		
+		
+		int sum = 0;
+		for(int i = 0; i < 100; i++){
+			if( i % 10 != 0 ){
+				sum += i;
+			}
+		}
+		
+		System.out.print(sum);
+	}
+}
