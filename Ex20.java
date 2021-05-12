6fb26357fb3567938a17376b515397af50fae2cf
Misaki <80904934+misaki0710@users.noreply.github.com>
2021-05-12 15:13:05 +0900

    about method

---
+++ Ex20.java
@@ -0,0 +1,10 @@
+public class Ex20{
+	public static void main(String[] args){
+		add(100,20);
+		add(100,50);
+	}
+	public static void add(int x, int y){
+		int ans = x + y;
+		System.out.println(x + "+" + y + "=" + ans);
+	}
+}
