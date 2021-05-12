30d341baa5b5e58e66ebd5b8f00b745c5d410aa0
Misaki <80904934+misaki0710@users.noreply.github.com>
2021-05-12 14:44:38 +0900

    about method

---
+++ Ex18.java
@@ -0,0 +1,11 @@
+public class Ex18{
+	public static void main(String[] args){
+		System.out.println("Call the method.");
+		hello("Misaki");
+		System.out.println("That's all.");
+	}
+	
+	public static void hello(String name){
+		System.out.println("Hello!" + name + ".");
+	}
+}
