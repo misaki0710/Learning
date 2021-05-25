9606a25728366e1ea79d0d4bb84b8a6dbed8a5c0
Misaki <80904934+misaki0710@users.noreply.github.com>
2021-05-25 20:34:21 +0900

    Add files via upload

---
+++ Moyou03.java
@@ -0,0 +1,21 @@
+public class Moyou03{
+	public static void main(String[] args){
+		
+		int size = 10;
+		String maru = "Åõ";
+		
+		for(int i=0; i<size; i++){
+			int j;
+			
+			for(j=0; j<i; j++){
+				System.out.print(" ");
+			}
+			for(j=i; j<size; j++){
+				System.out.print(maru);
+			}
+			System.out.println(" ");
+			
+		}
+		
+	}
+}
