b0eeb07867248119dd5d6682dfdf060c1f8234e7
Misaki <80904934+misaki0710@users.noreply.github.com>
2021-06-02 16:53:50 +0900

    Add files via upload

---
+++ Main07.java
@@ -0,0 +1,9 @@
+public class Main07{
+	public static void main(String[] args){
+		
+		Hero h1 = new Hero("minato");
+		System.out.println(h1.name);
+		Hero h2 = new Hero();
+		System.out.println(h2.name);
+	}
+}
