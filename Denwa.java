c3cddd1d5c99ca5f04642bbff1dcf9785a728a68
Misaki <80904934+misaki0710@users.noreply.github.com>
2021-06-18 12:47:27 +0900

    Add files via upload

---
+++ Denwa.java
@@ -0,0 +1,6 @@
+public class Denwa{
+	String no;
+	public void tell(){
+		System.out.println("denwa dekimasu");
+	}
+}
--- KakuninQ1.java
+++ KakuninQ1.java
@@ -1,11 +1,8 @@
 public class KakuninQ1{
 	public static void main(String[] args){
-		
-		Gohan a = new Gohan();
-		
-		a.asaGohan();
-		a.hiruGohan();
-		a.yoruGohan();
-		
+		Keitai k = new Keitai();
+		k.tell();
+		k.mail();
+		k.listen();
 	}
 }
