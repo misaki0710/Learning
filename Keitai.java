1b22bfa796e708c288bbdb509fc812081bf4305f
Misaki <80904934+misaki0710@users.noreply.github.com>
2021-06-18 12:47:57 +0900

    Add files via upload

---
+++ Keitai.java
@@ -0,0 +1,11 @@
+public class Keitai extends Denwa implements Ongaku{
+	String address;
+	
+	public void mail(){
+		System.out.println("mail dekimasu");
+	}
+	
+	public void listen(){
+		System.out.println("ongaku ga kikemasu");
+	}
+}
---
+++ Ongaku.java
@@ -0,0 +1,3 @@
+public interface Ongaku{
+	void listen();
+}
