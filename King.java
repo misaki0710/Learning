311f14f6695a7c06cd8c6c029084716b34716363
Misaki <80904934+misaki0710@users.noreply.github.com>
2021-06-30 15:52:48 +0900

    Add files via upload

---
+++ King.java
@@ -0,0 +1,9 @@
+public class King{
+	public void talk(Hero h){
+		System.out.println("welcome " + h.getName());
+		System.out.println("ousama : nagatabi tukareta dearou");
+		System.out.println("mazu ha joukamachi wo mitekuru to yoi. deha mata aou");
+		h.bye();
+	}
+	
+}
