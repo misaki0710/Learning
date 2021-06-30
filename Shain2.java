ff9b051f7186c24acd966d553d893db97b930c4f
Misaki <80904934+misaki0710@users.noreply.github.com>
2021-06-30 16:50:16 +0900

    Add files via upload

---
+++ Shain2.java
@@ -0,0 +1,25 @@
+class Shain2{
+	private int shainBangou;
+	private String busho;
+	private String namae;
+	private int kihonkyu;
+	
+	public Shain2(int shainBangou,String busho,String namae,int kihonkyu){
+		this.shainBangou = shainBangou;
+		this.busho = busho;
+		this.namae = namae;
+		this.kihonkyu = kihonkyu;
+	}
+	public int getShainBangou(){
+		return this.shainBangou;
+	}
+	public String getBusho(){
+		return this.busho;
+	}
+	public String getNamae(){
+		return this.namae;
+	}
+	public int getKihonkyu(){
+		return this.kihonkyu;
+	}
+}
