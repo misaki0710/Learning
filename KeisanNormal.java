a3d98f41a5935ad46b54f0b0e5b44833e5198b8f
Misaki <80904934+misaki0710@users.noreply.github.com>
2021-07-28 16:44:40 +0900

    Add files via upload

---
+++ KeisanNormal.java
@@ -0,0 +1,8 @@
+public class KeisanNormal{
+	int tashizan(int su1,int su2){
+		return su1 + su2;
+	}
+	int hikizan(int su1,int su2){
+		return su1 - su2;
+	}
+}
---
+++ KeisanStatic.java
@@ -0,0 +1,8 @@
+public class KeisanStatic{
+	static int tashizan(int su1,int su2){
+		return su1 + su2;
+	}
+	static int hikizan(int su1,int su2){
+		return su1 - su2;
+	}
+}
