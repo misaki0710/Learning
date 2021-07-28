ec96514be9f281d89ab9c1c2d1292ee93c8bf3ba
Misaki <80904934+misaki0710@users.noreply.github.com>
2021-07-28 16:45:27 +0900

    Add files via upload

---
+++ Sougou03Q12.java
@@ -0,0 +1,13 @@
+public class Sougou03Q12{
+	public static void main(String[] args){
+		Shisokuenzan s = new Shisokuenzan();
+		s.tashizan(15,5);
+		s.hyoji();
+		s.hikizan(15,5);
+		s.hyoji();
+		s.kakezan(15,5);
+		s.hyoji();
+		s.warizan(15,5);
+		s.hyoji();
+	}
+}
